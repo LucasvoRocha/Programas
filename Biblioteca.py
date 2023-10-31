d25be76404a566f405370c4e4db7e5c6e1bfd6f2
Lucas Rocha <144575286+LucasvoRocha@users.noreply.github.com>
2023-10-31 16:31:10 -0300

    Update Biblioteca.py

--- Biblioteca.py
+++ Biblioteca.py
@@ -95,3 +95,77 @@ def repeticao(list):
         if x not in list2:
             list2.append(x)
     print(list2)
+
+#Conta bancaria============================================
+
+class contabancaria:
+    def __init__(self, numero, saldo, nome, tipo, limite):
+        self.numero = numero
+        self.saldo = saldo
+        self.nome = nome
+        self.tipo = tipo
+        self.status = "inativa"
+        self.limite = limite
+
+    def ativar_conta(self):
+        self.status = "ativa"
+
+    def desativar_conta(self):
+        self.status = "inativa"
+
+    def depositar(self, valor):
+        if self.status == "ativa" and valor > 0:
+            self.saldo += valor
+            return f"Depósito de R${valor} realizado. Novo saldo: R${self.saldo}"
+        else:
+            return "Não é possível realizar o depósito."
+
+    def sacar(self, valor):
+        if self.status == "ativa" and 0 < valor <= (self.saldo + self.limite):
+            self.saldo -= valor
+            return f"Saque de R${valor} realizado. Novo saldo: R${self.saldo}"
+        else:
+            return "Não é possível realizar o saque."
+
+    def verificar_saldo(self):
+        if self.status == "ativa":
+            return f"Saldo disponível: R${self.saldo}"
+        else:
+            return "Conta inativa. Não é possível verificar o saldo."
+
+#animal==================================
+
+class animal():
+    def __init__(self, nome, cor):
+        self.nome = nome
+        self.cor = cor
+
+    def comer(self):
+        print(f"O {self.nome} foi comer...")
+
+    def emitir_som(self):
+        print(f"O {self.nome} esta emitindo som...")
+#subclasse
+class gato(animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+class vaca(animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+class boi(animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+class cachorro(animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+class galinha(animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+class masqueico(animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
